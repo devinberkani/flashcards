6d9528775b9356bc8859bc0822a057aeb8f6dfc7
Devin Berkani <devinberkani@gmail.com>
2022-09-09 19:09:38 -0400

    create ability for user to set flashcards and test knowledge

---
+++ Flashcards/src/Deck.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Flashcard> deck = new ArrayList<>();
+    public Deck() {
+
+    }
+
+    // getters and setters
+
+    protected ArrayList<Flashcard> getDeck() {
+        return deck;
+    }
+
+    private void setDeck(ArrayList<Flashcard> deck) {
+        this.deck = deck;
+    }
+}
